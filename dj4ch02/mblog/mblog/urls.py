0d677cd4369c2658087be02a799b550c1261b1bd
silviahuang0516 <76055567+silviahuang0516@users.noreply.github.com>
2024-06-26 16:01:45 +0800

    Delete dj4ch02 directory

--- dj4ch02/mblog/mblog/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from django.contrib import admin
-from mysite.views import homepage, showpost
-
-urlpatterns = [
-    path('', homepage),
-    path('post/<slug:slug>/', showpost),
-    path('admin/', admin.site.urls),
-]
